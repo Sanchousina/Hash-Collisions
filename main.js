ddf37bfcb6c73760670bced9898e493296269e59
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-12-08 12:55:00 +0100

    Read code file

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+import { readFile } from 'fs/promises';
+
 function hash(msg, output=2) {
   console.log(msg.length);
   const msgBytes = textToByteArray(msg);
@@ -28,4 +30,13 @@ function byteArrayToText(arr) {
   return str;
 }
 
-hash('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed nisl congue, congue leo vitae, vestibulum ligula');
+async function content(path) {
+  const data = await readFile(path, 'utf8');
+  return data;
+}
+
+const res = await content('./source.js');
+console.log(res);
+//console.log(text);
+
+//hash('Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque sed nisl congue, congue leo vitae, vestibulum ligula');
---
+++ source.js
@@ -0,0 +1,21 @@
+const { mod } = require('./util');
+
+function caesarCipher(msg, key, alphabet, n, type='encrypt') {
+  let messageArr = msg.split('');
+
+  let encryptedMessageArr = messageArr.map((char, i) => {
+
+    let charIndex = alphabet.indexOf(char.toLowerCase());
+
+    if (charIndex != -1) {
+      let encryptedCharIndex = type === 'decrypt' ? mod((charIndex - key),n) : (charIndex + key) % n;
+      return char === char.toUpperCase() ? alphabet[encryptedCharIndex].toUpperCase() : alphabet[encryptedCharIndex]
+    } else {
+      return msg[i];
+    }
+  })
+
+  return encryptedMessageArr.join('');
+}
+
+module.exports = {caesarCipher};
