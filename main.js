651587f37788b837e6d65c37cb8009db1da0b381
Oleksandra Okhotnykova <ohotnikova.sasha2002@gmail.com>
2023-12-08 11:49:47 +0100

    Add text to byte array funciton and vice versa

---
+++ main.js
@@ -0,0 +1,16 @@
+function hash(msg, output=2) {
+  
+}
+
+function textToByteArray(str) {
+  const utf8EncodeText = new TextEncoder();
+  const byteArray = utf8EncodeText.encode(str); // Uint8Array
+  return byteArray;
+}
+
+function byteArrayToText(arr) {
+  let uint8Array = new Uint8Array(arr);
+  let decoder = new TextDecoder("utf-8"); 
+  let str = decoder.decode(uint8Array); 
+  return str;
+}
